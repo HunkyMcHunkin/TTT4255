942e2a3e5c3654bd6cf0a24a3eb5d61211e35b45
Christian Nguyen <44482020+HunkyMcHunkin@users.noreply.github.com>
2019-06-11 12:55:29 +0200

    Add files via upload

---
+++ LCD-display.ino
@@ -0,0 +1,28 @@
+  #include <LiquidCrystal.h>
+  LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+  const int sensorPin = A0;
+  float sensorVal, temp;
+  
+void setup() {
+  // put your setup code here, to run once:
+  lcd.begin(16,2);
+
+  pinMode(sensorPin, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  sensorVal = analogRead(sensorPin);
+  temp = sensorVal * (5.0/1024);
+  temp = (temp - 0.5) * 100;
+
+  lcd.clear();
+  lcd.print("Temperatur:");
+  lcd.setCursor(0,1);
+  lcd.print(temp);
+  lcd.print(" C");
+  lcd.setCursor(0,0);
+
+  delay(1000);
+}
