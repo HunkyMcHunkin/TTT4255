771ff5739c48e505ec1117baf954db3f655781b6
Christian Nguyen <44482020+HunkyMcHunkin@users.noreply.github.com>
2019-06-24 10:39:50 +0200

    Update KommunikasjonArduinokort.ino

--- ekstraOppgaver/KommunikasjonArduinokort.ino
+++ ekstraOppgaver/KommunikasjonArduinokort.ino
@@ -3,18 +3,20 @@ int receivedData;
 
 void setup() {
   // put your setup code here, to run once:
+  pinMode(buttonPin, INPUT);
   Serial.begin(9600);
   while(!Serial){}
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if (Serial.available() > 0) {
+  if (Serial.available() > 1) {
     receivedData = Serial.parseInt();
 
     Serial.print("Teller ned fra ");
     Serial.print(receivedData, DEC);
     Serial.println(" når du trykker på knappen");
+    buttonWait(buttonPin);
   }
 
   if (digitalRead(buttonPin) == HIGH) {
@@ -24,4 +26,14 @@ void loop() {
     }
   }
   delay(200);
+}
+
+void buttonWait(int buttonPin){
+  int buttonState = 0;
+  while(1){
+    buttonState = digitalRead(buttonPin);
+    if (buttonState == HIGH) {
+      return;
+    }
+  }
 }
